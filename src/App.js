1b9d2d1404a9ef1ed0b90d0e22fc952f0ad7017d
Harsh Kumar Mishra <ha20312030@wipro.com>
2024-03-22 13:33:53 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
   const [value, setValue] = useState("");
   const [isClicked, setIsClicked] = useState(false);
   const [showAnimation, setShowAnimation] = useState(false);
-  const shareUrl = "https://google.com";
+  const shareUrl = "https://holi-fest-joy.netlify.app/";
   const shareTitle = "Check out this awesome website!";
 
   return (
     <div className={!showAnimation ? "bg" : value ? "bg__gif" : "bg"}>
-      <div className="w-[95%] lg:w-[90%] mx-auto ">
+      <div className="w-[95%] lg:w-[90%] h-[100vh] mx-auto ">
         <Header
           setValue={setValue}
           setIsClicked={setIsClicked}
